17327947653ee0332a1a14190cda31cbfebe8339
Robb Shecter <robb@weblaws.org>
2016-06-26 22:17:03 -0700

    Add callback function to auth controller

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -4,4 +4,10 @@ defmodule Ketobit.AuthController do
   def index(conn, _params) do
     redirect conn, external: Fitbit.authorize_url!(scope: "nutrition")
   end
+
+  def callback(conn, %{"code" => code}) do
+    token = Fitbit.get_token!(code: code)
+    IO.inspect token
+    redirect conn, to: "/"
+  end
 end
