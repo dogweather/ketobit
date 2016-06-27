af022b6fd03fe4df78b2d4bbdfdb73b05aab53db
Robb Shecter <robb@weblaws.org>
2016-06-26 21:23:56 -0700

    Trying to fix prod secret

--- config/prod.exs
+++ config/prod.exs
@@ -21,12 +21,6 @@ config :ketobit, Ketobit.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-# Configure your database
-config :ketobit, Ketobit.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
 
 # ## SSL Support
 #
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,1 +1,8 @@
-# No secrets here.
+use Mix.Config
+
+# Configure the database
+config :ketobit, Ketobit.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
