373a5e5eaf341ab93d28f015ddb8fda87e0302e3
Robb Shecter <robb@weblaws.org>
2016-07-03 23:56:09 -0700

    Retrieving the user's name

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,22 +2,27 @@ defmodule Ketobit.AuthController do
   use Ketobit.Web, :controller
 
   def index(conn, _params) do
-    redirect conn, external: Fitbit.authorize_url!(scope: "nutrition")
+    redirect conn, external: Fitbit.authorize_url!(scope: "profile nutrition")
   end
 
   def callback(conn, %{"code" => code}) do
     token = Fitbit.get_token!(code: code)
-    IO.inspect token
 
-    changeset = Ketobit.User.changeset(%Ketobit.User{},
+    data = OAuth2.AccessToken.get!(token, "/1/user/-/profile.json")
+    IO.inspect data
+
+    user_name = data["user"]["displayName"]
+
+    changeset = User.changeset(%User{},
       %{user_id: token.other_params["user_id"],
         access_token: token.access_token,
-        refresh_token: token.refresh_token
+        refresh_token: token.refresh_token,
+        name: user_name
       })
     Repo.insert!(changeset)
 
     conn
-      |> put_flash(:info, "User created successfully.")
+      |> put_flash(:info, "Hello #{user_name}!")
       |> redirect(to: "/")
   end
 end
