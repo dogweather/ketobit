cc03f9e228afb70fe6af44c1a2ca6693298da8f6
Robb Shecter <dogweather@users.noreply.github.com>
2016-07-04 20:30:15 -0700

    Show carbs remaining for the day (#1)
    * First pass at date/time code
    
    * Displays net carbs remaining
    
    * Layout

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Ketobit.Mixfile do
   def application do
     [mod: {Ketobit, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :oauth2]]
+                    :phoenix_ecto, :postgrex, :oauth2, :timex]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +37,8 @@ defmodule Ketobit.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
-     {:oauth2, "~> 0.2"}]
+     {:oauth2, "~> 0.2"},
+     {:timex, "~> 2.0"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,5 +1,6 @@
 defmodule Ketobit.AuthController do
   use Ketobit.Web, :controller
+  use Timex
   alias Ketobit.User
 
   def index(conn, _params) do
@@ -16,24 +17,39 @@ defmodule Ketobit.AuthController do
   def callback(conn, %{"code" => code}) do
     token = Fitbit.get_token!(code: code)
 
-    data = OAuth2.AccessToken.get!(token, "/1/user/-/profile.json").body
-    IO.inspect data
-
-    user_name = data["user"]["displayName"]
+    user_data = OAuth2.AccessToken.get!(token, "/1/user/-/profile.json").body
+    user_name = user_data["user"]["displayName"]
     user_id   = token.other_params["user_id"]
 
-    changeset = User.changeset(%User{},
-      %{user_id: user_id,
-        access_token: token.access_token,
-        refresh_token: token.refresh_token,
-        name: user_name
-      })
-    Repo.insert!(changeset)
+    # So far, we don't need to actually save any info in the database.
+    #
+    # changeset = User.changeset(%User{},
+    #   %{user_id: user_id,
+    #     access_token: token.access_token,
+    #     refresh_token: token.refresh_token,
+    #     name: user_name
+    #   })
+    # Repo.insert!(changeset)
+
+    # Get today's food log
+    # TODO: Ensure this is the *user's* current date (& timezone).
+    today    = DateTime.today
+    timezone = Timex.timezone("America/Los_Angeles", today)
+    {:ok, today_8601} = today
+      |> Timezone.convert(timezone)
+      |> Timex.format("%Y-%m-%d", :strftime)
+    food_log = OAuth2.AccessToken.get!(token, "/1/user/-/foods/log/date/#{today_8601}.json").body
+
+    # Calculate the info we're interested in
+    net_carbs   = food_log["summary"]["carbs"] - food_log["summary"]["fiber"]
+    keto_budget = 20 - net_carbs
 
     conn
       |> put_flash(:info, "Hello #{user_name}!")
       |> put_session(:current_user, user_id)
       |> put_session(:access_token, token.access_token)
+      |> put_session(:net_carbs, net_carbs)
+      |> put_session(:keto_budget, keto_budget)
       |> redirect(to: "/")
   end
 end
