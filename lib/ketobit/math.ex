9b5dd6d8d44a0f224838c925e5010d87546b18e9
Robb Shecter <robb.shecter@gsa.gov>
2016-07-24 19:01:03 -0700

    Better rounding

--- lib/ketobit/math.ex
+++ lib/ketobit/math.ex
@@ -2,7 +2,7 @@ defmodule Ketobit.Math do
 
   # Format an int or float safely to one decimal point.
   def round_decimal(number) do
-    Float.floor(number / 1, 1)
+    round(number * 10) / 10
   end
 
 end
--- test/lib/ketobit/math_test.exs
+++ test/lib/ketobit/math_test.exs
@@ -2,13 +2,23 @@ defmodule Ketobit.MathTest do
   use Ketobit.ModelCase
 
   test "round_decimal handles integers" do
-    rounded = Ketobit.Math.round_decimal(5)
-    assert 5.0 = rounded
+    rounded = Ketobit.Math.round_decimal(20)
+    assert 20.0 = rounded
   end
 
-  test "round_decimal simply truncates" do
+  test "round_decimal rounds up" do
     rounded = Ketobit.Math.round_decimal(5.99)
-    assert 5.9 = rounded
+    assert 6.0 = rounded
+  end
+
+  test "round_decimal rounds down" do
+    rounded = Ketobit.Math.round_decimal(5.14147469)
+    assert 5.1 = rounded
+  end
+
+  test "round_decimal handles zero" do
+    rounded = Ketobit.Math.round_decimal(0)
+    assert 0.0 = rounded
   end
 
 end
