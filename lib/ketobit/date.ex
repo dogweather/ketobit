c9dcd0ad7c310c84072adbdaf5403c567beec56a
Robb Shecter <robb.shecter@gsa.gov>
2016-07-24 19:45:17 -0700

    iso_8601 pattern matching

--- lib/ketobit/date.ex
+++ lib/ketobit/date.ex
@@ -1,7 +1,7 @@
 defmodule Ketobit.Date do
   use Timex
 
-  def iso_8601(date) do
+  def iso_8601(date = %DateTime{}) do
     {:ok, iso_8601} = Timex.format(date, "%Y-%m-%d", :strftime)
     iso_8601
   end
